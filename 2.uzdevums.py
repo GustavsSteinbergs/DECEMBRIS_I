e9783e562a5098a8c7970951d0a85b96a494d897
Gustavs <gustavssteinbergs@grobinasskola.com>
2023-11-24 09:06:44 +0200

    https://github.com/GustavsSteinbergs/DECEMBRIS_I.git

---
+++ 2.uzdevums.py
@@ -0,0 +1,11 @@
+# Izveidojiet Python programmu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts nav lielāks par 1000.
+
+# Sākumskaitlis.
+skaitlis = 1
+
+# Izmantoju While ciklu, kurš turpinās, kamēr kvadrāts ir lielāks par 1000.
+while skaitlis**2 <= 1000:
+    skaitlis += 1
+
+# Izvadē tiek parādīts/uzrādīts rezultāts.
+print(f"Pirmāis skaitlis, kura kvadrāts nav lielāks par 1000, ir: {skaitlis}")
