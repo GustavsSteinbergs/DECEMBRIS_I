c55c0a16b17b25918a1a0d00f4c2717ac316f907
Gustavs <gustavssteinbergs@grobinasskola.com>
2023-11-24 09:35:26 +0200

    https://github.com/GustavsSteinbergs/DECEMBRIS_I.git

--- 1.uzdevums.py
+++ 1.uzdevums.py
@@ -1,10 +1,15 @@
 # Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
 
-
+#Lietotāju ievade 
 lietotaja_ievade = int(input("Ievadiet skaitli: "))
+
+# Saskaitīšanas mainīgais
 summa = 0
 
+# For cikls izmantojam mēs, lai saskaitītu no 1 līdz ievadītajam skaitlim
 for i in range(1, lietotaja_ievade + 1):
     summa += i
 
+# Izvadē jeb terminālī tiek parādīta saskaitīto skaitļu summa
 print(f"Saskaitot no 1 līdz {lietotaja_ievade}, rezultāts ir: {summa}")
+
